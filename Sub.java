a52d07126acc3dfa262ecc22866ac72fad552c00
KUMARIKAJOL <122786014+KUMARIKAJOL@users.noreply.github.com>
2023-01-23 13:49:43 +0530

    Add files via upload

---
+++ Sub.java
@@ -0,0 +1,17 @@
+class Mul{
+
+public static void main(String[]args){
+
+System.out.println("The two number is :");
+
+int a = 5; 
+
+int b = 6; 
+
+System.out.println(a + "  " + b); 
+
+int Mul = a*b;
+
+System.out.println("The Mul is = " +  Mul);
+}
+}    
