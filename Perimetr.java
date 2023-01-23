801416e6260421be12744ef256524fe20888dea3
KUMARIKAJOL <122786014+KUMARIKAJOL@users.noreply.github.com>
2023-01-23 13:46:56 +0530

    Add files via upload

---
+++ Perimetr.java
@@ -0,0 +1,11 @@
+class Area{
+
+public static void main(String []args){
+System.out.println(" lenght and breath is: ");
+int l= 10;
+int b = 20;
+System.out.println(l + "  " + b);  
+int Area = l+b 
+System.out.println("the Perimetr of retangle is: " + Area);
+}
+}
