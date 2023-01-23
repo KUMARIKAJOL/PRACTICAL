8e8709fd27bbea1d96669e0d8a0315c06c676bad
KUMARIKAJOL <122786014+KUMARIKAJOL@users.noreply.github.com>
2023-01-23 13:47:22 +0530

    Add files via upload

---
+++ Assingnment.java
@@ -0,0 +1,14 @@
+class Assingnment {
+
+public static void main(String[] args){
+
+int a=50;
+a+=3;
+System.out.println(a);
+a-=10;
+System.out.println(a);
+a*=2;
+System.out.println(a);
+a/=2;
+System.out.println(a);
+}}
