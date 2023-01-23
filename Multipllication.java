c0deb6e1f0f7e9686dc0a0f70dcfd1a69821d352
KUMARIKAJOL <122786014+KUMARIKAJOL@users.noreply.github.com>
2023-01-23 13:48:52 +0530

    Add files via upload

---
+++ Multipllication.java
@@ -0,0 +1,17 @@
+class Multipllication{
+
+public static void main(String[]args){
+
+System.out.println("The two number is :");
+
+int a = 5; 
+
+int b = 6; 
+
+System.out.println(a + "  " + b); 
+
+int Multipllication = a*b;
+
+System.out.println("The Multipllication is = " +  Multi);
+}
+}    
