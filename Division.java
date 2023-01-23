51fb8ac6b7f7df1f94d781fc42e8d03804afb9a5
KUMARIKAJOL <122786014+KUMARIKAJOL@users.noreply.github.com>
2023-01-23 13:50:17 +0530

    Add files via upload

---
+++ Division.java
@@ -0,0 +1,17 @@
+class Division {
+
+public static void main(String[] args){
+
+int a =60;
+
+int b =30;
+
+int c= a/b;
+
+System.out.println("  first nuber : " + a);
+
+System.out.println(" second number : " + b);
+
+System.out.println(" The Division is = " + c);
+}
+}
