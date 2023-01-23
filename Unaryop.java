e16995e89f54283659f4485a7ad5ae887a30e868
KUMARIKAJOL <122786014+KUMARIKAJOL@users.noreply.github.com>
2023-01-23 15:20:48 +0530

    Add files via upload

---
+++ Unaryop.java
@@ -0,0 +1,14 @@
+class Unaryop{
+public static void main(String[] args){
+
+int a=5;
+
+//prostix & prefix for increment
+System.out.println(a++);
+System.out.println(++a);
+
+//profix & prefix for Decrement
+System.out.println(a--);
+System.out.println(--a);
+}}
+
