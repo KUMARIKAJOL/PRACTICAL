9ea3e9edc82cc63fc05ebf7099ac719a23a65e27
KUMARIKAJOL <122786014+KUMARIKAJOL@users.noreply.github.com>
2023-01-24 12:05:31 +0530

    Add files via upload

---
+++ Math.java
@@ -0,0 +1,12 @@
+class Math{
+public static void main(String[] args){
+int a = 2;
+int b = 3;
+System.out.println(a>b&&b<a);
+System.out.println(a<b&&b<a);
+System.out.println(a<b&&b==a);
+
+System.out.println(a>b||b<a);
+System.out.println(a<b||b<a);
+System.out.println(a>b||b<a);
+}}
