374f2f1397ff586816d628f05e512643ce767d58
KUMARIKAJOL <122786014+KUMARIKAJOL@users.noreply.github.com>
2023-01-31 17:49:59 +0530

    Add files via upload

---
+++ Dowhileloop2.java
@@ -0,0 +1,15 @@
+package controlstatement;
+
+public class Dowhileloop2 {
+	public static void main(String[] args) {
+		int i = 1;
+		System.out.println("First 10 odd numbers \n");
+		do {
+			System.out.println(i);// statment
+			i = i+2; //increment
+		}while(i<=30); //condition
+		
+		}
+	}
+
+
