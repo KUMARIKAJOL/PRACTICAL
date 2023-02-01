ca70d37362552dd8826cc538ba4b0f518fb7f827
KUMARIKAJOL <122786014+KUMARIKAJOL@users.noreply.github.com>
2023-02-01 12:16:42 +0530

    Add files via upload

---
+++ Dowhileeg.java
@@ -0,0 +1,20 @@
+package controlstatement;
+
+public class Dowhileeg {
+	public static void main(String[] args) {
+	int i = 1;
+		do {
+			System.out.println();// statment
+			i++;
+		}while(i<=10);//condition (exit cntrol lop)
+	 	
+		
+		do {
+			System.out.println("chalte raho");// statment
+			
+		}while(true); //infinete do while
+			
+		}
+	}
+
+ 
