890ea7b735b81b76c466ef845bccd5655f5811e5
KUMARIKAJOL <122786014+KUMARIKAJOL@users.noreply.github.com>
2023-02-01 14:16:14 +0530

    Add files via upload

---
+++ Whileloop.java
@@ -0,0 +1,19 @@
+package controlstatement;
+
+public class Whileloop {
+	public static void main(String[] args) {
+		
+		int i = 1;
+		//while(i<=10){ //conditon at last
+		//System.out.println(i);
+		while(true){  // infinite while loop
+			
+			System.out.println(i);
+			
+			
+	  i++;
+		
+		
+	}
+
+}}
