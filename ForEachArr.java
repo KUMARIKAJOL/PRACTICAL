31609600e09344a1a804fac53497be782cbbbb0a
KUMARIKAJOL <122786014+KUMARIKAJOL@users.noreply.github.com>
2023-02-01 12:18:45 +0530

    Add files via upload

---
+++ ForEachArr.java
@@ -0,0 +1,16 @@
+package controlstatement;
+
+public class ForEachArr {
+	public static void main(String[] args) {
+		// declear an array
+		//int arr[]= {12,22,45,25,32};
+		//using for each loop for print the array
+		//for(int i:arr) {
+		//	System.out.println(i);
+		//}
+		for(;; ) { //infinitive (ctrl+c)
+			System.out.println("chalte raho");
+		}
+	}
+
+}
