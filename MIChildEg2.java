31e3b0fcef82993e3a5e49ed2ab72f3b7e4c9c08
KUMARIKAJOL <122786014+KUMARIKAJOL@users.noreply.github.com>
2023-02-13 17:32:40 +0530

    Add files via upload

---
+++ MIChildEg2.java
@@ -0,0 +1,24 @@
+package kajol.com;
+class MLIParenteg3{ // parent class
+	int salary =30000; // parent class member
+}
+class MIChildEg1 extends MLIParenteg3 {
+	int bonus= 1500; // child clas member
+	int annualsal= salary+ bonus;
+	
+}
+class MIChildEg2 extends MIChildEg1 {
+	int da = 200;
+	int totalsal = annualsal+da;
+	
+	public static void main(String[] args) {
+	MIChildEg2 object = new MIChildEg2();
+	System.out.println("Salaray is;"+ object.salary);
+	System.out.println("Annual Salary is:"+ object.annualsal);
+	System.out.println("Total Salary is:"+ object.totalsal);
+	}}
+
+
+ 
+
+
